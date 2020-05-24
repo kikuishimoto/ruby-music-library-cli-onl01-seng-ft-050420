870a94c205c0f54f8604bd003fa7ae0744c1d110
kiku ishimoto <kikuish@gmail.com>
2020-05-24 06:50:03 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -40,7 +40,7 @@ class MusicLibraryController
   end
 
   def list_songs_by_artist
-    Artist.@songs.sort{|a, b| a.name <=> b.name}.each.with_index(1) do |s, i|
+    Artist.songs.sort{|a, b| a.name <=> b.name}.each.with_index(1) do |s, i|
       puts "#{i}, #{g.name}"
     end 
   end
