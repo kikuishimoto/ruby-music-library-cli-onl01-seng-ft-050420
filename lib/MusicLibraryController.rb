b124f233dcf3d7cb22fde16118930d326d03d76e
kiku ishimoto <kikuish@gmail.com>
2020-05-24 06:57:22 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -44,7 +44,7 @@ class MusicLibraryController
     input = gets.strip
     if artist = Artist.find_by_name(input)
       artist.songs.sort{|a, b| a.name <=> b.name}.each.with_index(1) do |s, i|
-        puts "#{i}, #{s.name}"
+        puts "#{i}, #{s.name} - #{s.genre.name}"
       end
     end
   end
