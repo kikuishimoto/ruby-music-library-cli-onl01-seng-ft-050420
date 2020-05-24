2110b73e583344345f9e1f90bfcb3fb07e6d9c15
kiku ishimoto <kikuish@gmail.com>
2020-05-24 07:10:01 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -20,6 +20,19 @@ class MusicLibraryController
       puts "What would you like to do?"
 
       input = gets.strip
+
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      
     end
   end
 
